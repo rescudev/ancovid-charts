4132d91f018df34dee8a1f4f1456948dbadf46a1
rescudev <rafael.escudero@optimaonline.es>
2020-05-25 12:50:16 +0200

    Added functionality and dump
    scraper now calls the API to add new prevalencias, added a mongoDB dump as an example.

--- mean-scraper/scrapers.js
+++ mean-scraper/scrapers.js
@@ -1,4 +1,6 @@
 const puppeteer = require('puppeteer');
+const axios = require('axios');
+const XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 
 async function scrapePrevalencia(url) {
         const browser = await puppeteer.launch({});
@@ -40,6 +42,8 @@ async function scrapePrevalencia(url) {
         
         console.log({obj});
         await browser.close();
+
+        return obj;
 }
 
 function addComunidad(rawTxt, comunidad, siglas, siguiente) {
@@ -51,8 +55,53 @@ function addComunidad(rawTxt, comunidad, siglas, siguiente) {
         return res;
 }    
 
-scrapePrevalencia('https://www.juntadeandalucia.es/organismos/saludyfamilias/actualidad/noticias/detalle/236172.html');
-scrapePrevalencia('https://www.juntadeandalucia.es/organismos/saludyfamilias/actualidad/noticias/detalle/236181.html');
-scrapePrevalencia('https://www.juntadeandalucia.es/organismos/saludyfamilias/actualidad/noticias/detalle/236254.html');
-scrapePrevalencia('https://www.juntadeandalucia.es/organismos/saludyfamilias/actualidad/noticias/detalle/236338.html');
-scrapePrevalencia('https://www.juntadeandalucia.es/organismos/saludyfamilias/actualidad/noticias/detalle/236457.html');
+async function postPrevalenciaJSON(url) {
+        var json = await scrapePrevalencia(url);
+        axios.post('http://localhost:3000/prevalencia', {
+                Fecha: json.Fecha,
+                ANHosp: json.ANHosp,
+                ANUCI: json.ANUCI,
+                ALHosp: json.ALHosp,
+                ALUCI: json.ALUCI,
+                CAHosp: json.CAHosp,
+                CAUCI: json.CAUCI,
+                COHosp: json.COHosp,
+                COUCI: json.COUCI,
+                GRHosp: json.GRHosp,
+                GRUCI: json.GRUCI,
+                HUHosp: json.HUHosp,
+                HUUCI: json.HUUCI,
+                JAHosp: json.JAHosp,
+                JAUCI: json.JAUCI,
+                MAHosp: json.MAHosp,
+                MAUCI: json.MAUCI,
+                SEHosp: json.SEHosp,
+                SEUCI: json.SEUCI
+        })
+        .then((res) => {
+        console.log(`statusCode: ${res.statusCode}`)
+        console.log(res)
+        })
+        .catch((error) => {
+        console.error(error)
+        })
+}
+
+var prevURL_16_05 = 'https://www.juntadeandalucia.es/organismos/saludyfamilias/actualidad/noticias/detalle/236172.html';
+var prevURL_17_05 = 'https://www.juntadeandalucia.es/organismos/saludyfamilias/actualidad/noticias/detalle/236181.html';
+var prevURL_18_05 = 'https://www.juntadeandalucia.es/organismos/saludyfamilias/actualidad/noticias/detalle/236254.html';
+var prevURL_19_05 = 'https://www.juntadeandalucia.es/organismos/saludyfamilias/actualidad/noticias/detalle/236338.html';
+var prevURL_20_05 = 'https://www.juntadeandalucia.es/organismos/saludyfamilias/actualidad/noticias/detalle/236457.html';
+
+var prevURL_21_05 = 'https://www.juntadeandalucia.es/organismos/saludyfamilias/actualidad/noticias/detalle/236522.html';
+var prevURL_22_05 = 'https://www.juntadeandalucia.es/organismos/saludyfamilias/actualidad/noticias/detalle/236605.html';
+var prevURL_23_05 = 'https://www.juntadeandalucia.es/organismos/saludyfamilias/actualidad/noticias/detalle/236622.html';
+var prevURL_24_05 = 'https://www.juntadeandalucia.es/organismos/saludyfamilias/actualidad/noticias/detalle/236629.html';
+
+postPrevalenciaJSON(prevURL_21_05);
+postPrevalenciaJSON(prevURL_22_05);
+postPrevalenciaJSON(prevURL_23_05);
+postPrevalenciaJSON(prevURL_24_05);
+
+
+
