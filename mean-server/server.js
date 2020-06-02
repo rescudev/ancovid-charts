edc1c237636a202f3b7134da0865ad9d61609bd7
rescudev <rafael.escudero@optimaonline.es>
2020-06-02 22:45:14 +0200

    Finally Deployed!!!!

--- mean-server/server.js
+++ mean-server/server.js
@@ -3,6 +3,7 @@ const app = express();
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const path = require('path');
 require('dotenv/config');
 
 //Middlewares
@@ -16,6 +17,8 @@ const hospitalizadoRoute = require('./routes/hospitalizado');
 const residenciaRoute = require('./routes/residencia');
 const territorioRoute = require('./routes/territorio');
 
+const PORT = process.env.PORT || 8080;
+
 app.use('/prevalencia', prevalenciaRoute);
 app.use('/pcr', pcrRoute);
 app.use('/hospitalizado', hospitalizadoRoute);
@@ -23,16 +26,25 @@ app.use('/residencia', residenciaRoute);
 app.use('/territorio', territorioRoute);
 
 //ROUTES
-app.get('/', (req, res) => {
-    res.send('We are on home');
+// app.get('/', (req, res) => {
+//     res.send('We are on home');
+// });
+
+app.use(express.static('./mean-web/dist/mean-web'));
+
+app.get('*', (req, res) => {
+    res.sendFile('index.html', {root: 'mean-web/dist/mean-web/'});
 });
 
 //connect to db
 mongoose.connect(
+    process.env.MONGOLAB_URI,
+    { useNewUrlParser: true, useUnifiedTopology: true},() => 
+    console.log('Connected to db Heroku') ||
     process.env.DB_CONNECTION,
     { useNewUrlParser: true, useUnifiedTopology: true},() => 
-    console.log('Connected to db')
+    console.log('Connected to db directly')
 ); 
 
 //Listen to server
-app.listen(3000);
+app.listen(PORT, console.log('Server is starting at '+ PORT));
