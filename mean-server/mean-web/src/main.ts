13fdaca9dcbdd638b68bd38eff3792e36b537f83
rescudev <rafael.escudero@optimaonline.es>
2020-06-05 14:26:20 +0200

    Mobile compatibility
    Added screen asking mobile users to use landscape.

--- mean-server/mean-web/src/main.ts
+++ mean-server/mean-web/src/main.ts
@@ -10,3 +10,21 @@ if (environment.production) {
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
+
+  var orientation = (screen.orientation || {}).type;
+
+  if (orientation === "portrait-secondary" || orientation === "portrait-primary") {
+    window.location.href = "http://localhost:4200/assets/rotate.html";
+  } else if (orientation === undefined) {
+    console.log("The orientation API isn't supported in this browser :(");
+  }
+
+  window.addEventListener("orientationchange", function() {
+    console.log("the orientation of the device is now " + screen.orientation.angle);
+    if(screen.orientation.angle < 45){
+      window.location.href = "http://localhost:4200/assets/rotate.html";
+      console.log('aaa');
+    }else{
+      window.location.href = "http://localhost:4200/";
+    }
+  });
