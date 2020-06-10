cf7caa969e33d311b533c619b84f32068c5dd74e
rescudev <rafael.escudero@optimaonline.es>
2020-06-10 10:28:12 +0200

    release 0.4.0 ready!

--- mean-server/mean-web/src/app/services/data-api.service.ts
+++ mean-server/mean-web/src/app/services/data-api.service.ts
@@ -10,32 +10,32 @@ export class DataApiService {
   constructor(private http: HttpClient) { }
 
   getPrevalencia(){
-    const url_api = 'http://localhost:3000/prevalencia/chart';
+    const url_api = 'prevalencia/chart';
     return this.http.get(url_api);
   }
 
   getUci(){
-    const url_api = 'http://localhost:3000/prevalencia/chart';
+    const url_api = 'prevalencia/chart';
     return this.http.get(url_api);
   }
 
   getHospitalizados(){
-    const url_api = 'http://localhost:3000/hospitalizado/chart';
+    const url_api = 'hospitalizado/chart';
     return this.http.get(url_api);
   }
 
   getProvincias(){
-    const url_api = 'http://localhost:3000/territorio/chart';
+    const url_api = 'territorio/chart';
     return this.http.get(url_api);
   }
 
   getPcrs(Fecha){
-    const url_api = 'http://localhost:3000/pcr/chart/'+Fecha;
+    const url_api = 'pcr/chart/'+Fecha;
     return this.http.get(url_api);
   }
 
   getLastDate(){
-    const url_api = 'http://localhost:3000/pcr/date';
+    const url_api = 'pcr/date';
     return this.http.get(url_api);
   }
 
